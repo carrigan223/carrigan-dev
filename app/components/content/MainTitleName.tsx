0c130994deda252b3d9592f5f8cf00dfb73c1d66
Andrew <andrew@ripemetrics.com>
2024-03-12 01:53:29 -0700

    Add @emotion/react and react-awesome-reveal dependencies, update MainTitleName component

--- app/components/content/MainTitleName.tsx
+++ app/components/content/MainTitleName.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { use, useEffect, useState } from "react";
+import { AttentionSeeker, Fade } from "react-awesome-reveal";
+import { text } from "stream/consumers";
 
 type Props = {
   firstName: string;
@@ -6,23 +8,35 @@ type Props = {
 };
 
 const MainTitleName = ({ firstName, lastName }: Props) => {
+  const [textShadow, setTextShadow] = useState("none");
+  useEffect(() => {
+    setTimeout(() => {
+      setTextShadow("10px 16px 3px #39cbcb5e");
+    }, 1300);
+  }, []);
   return (
     <div className="flex flex-col w-max">
-      <h1
-        style={{
-          textShadow: "10px 18px 2px #39cbcb5e",
-        }}
-      >
-        {firstName}
-      </h1>
-      <h1
-        className="ml-12"
-        style={{
-          textShadow: "10px 18px 2px #39cbcb5e",
-        }}
-      >
-        {lastName}
-      </h1>
+      <AttentionSeeker effect="flash" triggerOnce delay={1300}>
+        <Fade direction="left" triggerOnce duration={1200}>
+          <h1
+            style={{
+              textShadow: textShadow,
+            }}
+          >
+            {firstName}
+          </h1>
+        </Fade>
+        <Fade direction="right" triggerOnce duration={1200}>
+          <h1
+            className="ml-12"
+            style={{
+              textShadow: textShadow,
+            }}
+          >
+            {lastName}
+          </h1>
+        </Fade>
+      </AttentionSeeker>
     </div>
   );
 };
